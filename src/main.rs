9bd6ca208f7de342fe5e9b72359e83a72ccca811
Corallus-Caninus <ward.joshua92@yahoo.com>
2021-08-29 05:42:57 -0700

    add keypad and arrow modes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use inputbot::{
 };
 //use tensorflow::ops::Enter;
 // use these to feed to Rat_Tunnel network and animate
-// motions such as lines to track tunnel cursor telepor
+// motions such as lines to track tunnel cursor teleport
 use x11::xlib::{XGetImage, XPutImage};
 use x11::{xinput2, xlib};
 //import crate for delay
@@ -12,6 +12,8 @@ use std::env;
 use std::thread::sleep;
 use std::time::Duration;
 use std::{self, primitive};
+use uinput;
+use uinput::event::keyboard;
 //import box
 use enclose::enclose;
 use std::boxed::Box;
@@ -29,14 +31,16 @@ struct Mouse_Action {
 trait MoveRat {
     fn move_rat(
         self,
-        is_numlock_on: Arc<Mutex<RefCell<Box<bool>>>>,
         is_fast: Arc<Mutex<RefCell<Box<bool>>>>,
         is_slow: Arc<Mutex<RefCell<Box<bool>>>>,
+        is_rat_on: Arc<Mutex<RefCell<Box<bool>>>>,
+        is_numlock_on: Arc<Mutex<RefCell<Box<bool>>>>,
         fast_speed: u64,
         medium_speed: u64,
         slow_speed: u64,
         mode_keypad: KeybdKey,
-        mode_arrow: KeybdKey,
+        // mode_arrow: KeybdKey,
+        mode_arrow: keyboard::Key,
         x: i32,
         y: i32,
     );
@@ -44,20 +48,22 @@ trait MoveRat {
 impl MoveRat for KeybdKey {
     fn move_rat(
         self,
-        is_numlock_on: Arc<Mutex<RefCell<Box<bool>>>>,
         is_fast: Arc<Mutex<RefCell<Box<bool>>>>,
         is_slow: Arc<Mutex<RefCell<Box<bool>>>>,
+        is_rat_on: Arc<Mutex<RefCell<Box<bool>>>>,
+        is_numlock_on: Arc<Mutex<RefCell<Box<bool>>>>,
         fast_speed: u64,
         medium_speed: u64,
         slow_speed: u64,
         mode_keypad: KeybdKey,
-        mode_arrow: KeybdKey,
+        // mode_arrow: KeybdKey,
+        mode_arrow: keyboard::Key,
         x: i32,
         y: i32,
     ) {
-        self.bind(enclose!((is_numlock_on, is_fast) move || {
-            if *is_numlock_on.lock().unwrap().borrow().clone() {
-                while self.is_pressed() {
+        self.bind(move || {
+            while self.is_pressed() {
+                if *is_rat_on.lock().unwrap().borrow().clone() {
                     let is_slow = *is_slow.lock().unwrap().borrow().clone();
                     let is_fast = *is_fast.lock().unwrap().borrow().clone();
                     //TODO: slow mode with mixing using plus key
@@ -65,29 +71,51 @@ impl MoveRat for KeybdKey {
                     if is_fast && is_slow {
                         //move up with fast speed
                         MouseCursor::move_abs(x, y);
-                        sleep(Duration::from_micros((medium_speed-fast_speed)/2 as u64)); //TODO
-                    }else if is_fast {
+                        sleep(Duration::from_micros(
+                            (medium_speed - fast_speed) / 2 as u64,
+                        ));
+                    } else if is_fast {
                         //move up with slow speed
                         MouseCursor::move_abs(x, y);
                         sleep(Duration::from_micros(fast_speed as u64));
-                    }
-                    else if is_slow {
+                    } else if is_slow {
                         //move up with slow speed
                         MouseCursor::move_abs(x, y);
                         sleep(Duration::from_micros(slow_speed as u64));
-                    }
-                    else {
+                    } else {
                         //move up with medium speed
                         MouseCursor::move_abs(x, y);
                         sleep(Duration::from_micros(medium_speed as u64));
                     }
+                } else if *is_numlock_on.lock().unwrap().borrow().clone() {
+                    //TODO: NKRO x11 writes
+                    //TODO: press and hold mode
+                    //TODO: consolidate this with inputbot in a way that is contributable
+                    &KEYBD_DEVICE.lock().unwrap().press(&mode_arrow).unwrap();
+                    &KEYBD_DEVICE.lock().unwrap().synchronize();
+                    &KEYBD_DEVICE.lock().unwrap().release(&mode_arrow).unwrap();
+                    &KEYBD_DEVICE.lock().unwrap().synchronize();
+                    sleep(Duration::from_micros(medium_speed as u64));
+                    //mode_arrow.press();
+                } else {
+                    //press and release arrow with medium speed
+                    // mode_keypad.click(Duration::from_micros(medium_speed as u64));
+                    mode_keypad.press();
+                    &KEYBD_DEVICE.lock().unwrap().synchronize();
+                    mode_keypad.release();
+                    &KEYBD_DEVICE.lock().unwrap().synchronize();
+                    sleep(Duration::from_micros(medium_speed as u64));
                 }
             }
-            //TODO: else .press(); arrow key or digit
-        }));
+        });
     }
 }
+//TODO: use led settings for custom blink codes or other modal user feedback
 fn main() {
+    //TODO: this is to focus the virtual device
+    AKey.release();
+    sleep(Duration::from_millis(100));
+
     let args = env::args().skip(1).collect::<Vec<String>>();
     let mut args = args
         .into_iter()
@@ -124,6 +152,7 @@ fn main() {
 
     // TODO: force this to sync with numlock on initialization
     let is_numlock_on = Arc::new(Mutex::new(RefCell::new(Box::new(true))));
+    let is_rat_on = Arc::new(Mutex::new(RefCell::new(Box::new(true))));
 
     //  Num_Lock can't keep up so we need to write our own stateful modes using different toggle keys
     let mut awaits = vec![];
@@ -154,9 +183,8 @@ fn main() {
                 .spawn(), //.output(),
         );
     }
-    //TODO: implement Enter key as slow mode with fast mode mixing
-    //TODO this may be marginal with above loop check during implementation
-    //TODO: this is a hack to prevent keypad from sending
+
+    //TODO: these are a hack to prevent keypad from sending
     awaits.push(
         std::process::Command::new("xmodmap")
             .args(&["-e", r#"keycode 104 = 1000"#]) //TODO: wrong
@@ -169,11 +197,11 @@ fn main() {
     //         .spawn(),
     // );
     //forward slash
-    // awaits.push(
-    //     std::process::Command::new("xmodmap")
-    //         .args(&["-e", r#"keycode 106 = 915"#]) //TODO: 98
-    //         .spawn(),
-    // );
+    awaits.push(
+        std::process::Command::new("xmodmap")
+            .args(&["-e", r#"keycode 106 = 915"#]) //TODO: 98
+            .spawn(),
+    );
     // enter
     // awaits.push(
     //     std::process::Command::new("xmodmap")
@@ -192,136 +220,135 @@ fn main() {
         x.unwrap();
     });
 
+    //TODO: diagonals with two arrows
     MouseKeyUp.move_rat(
         // cloning here is weird but doesnt really matter since this is config
         // and i'll take what I can get from the borrow checker
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad8Key,
-        UpKey,
+        Numrow8Key,
+        // UpKey,
+        keyboard::Key::Up,
         0,
         -1,
     );
     MouseKeyDown.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad2Key,
-        DownKey,
+        Numrow2Key,
+        // DownKey,
+        keyboard::Key::Down,
         0,
         1,
     );
     MouseKeyLeft.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad4Key,
-        LeftKey,
+        Numrow4Key,
+        // LeftKey,
+        keyboard::Key::Left,
         -1,
         0,
     );
     MouseKeyRight.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad6Key,
-        RightKey,
+        Numrow6Key,
+        // RightKey,
+        keyboard::Key::Right,
         1,
         0,
     );
     MouseKeyUpperLeft.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad7Key,
+        Numrow7Key,
         //TODO: this should be up and left at the same time
-        UpKey,
+        // UpKey,
+        keyboard::Key::Up,
         -1,
         -1,
     );
     MouseKeyUpperRight.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad9Key,
-        UpKey,
+        Numrow9Key,
+        // UpKey,
+        keyboard::Key::PageUp,
         1,
         -1,
     );
     MouseKeyLowerRight.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad3Key,
-        DownKey,
+        Numrow3Key,
+        // DownKey,
+        keyboard::Key::PageDown,
         1,
         1,
     );
     MouseKeyLowerLeft.move_rat(
-        is_numlock_on.clone(),
         is_fast.clone(),
         is_slow.clone(),
+        is_rat_on.clone(),
+        is_numlock_on.clone(),
         fast_speed as u64,
         medium_speed as u64,
         slow_speed as u64,
-        Numpad1Key,
-        DownKey,
+        Numrow1Key,
+        // DownKey,
+        keyboard::Key::Down,
         -1,
         1,
     );
 
-    //TODO: fix this naming in input keycode
-    NumpadPlusKey.bind(
-        enclose!((is_numlock_on=>is_numlock_on_click_toggle,left_click_toggle=> right_click_toggle) move || {
-            if *is_numlock_on_click_toggle.lock().unwrap().borrow().clone() {
-                MouseButton::RightButton.press();
-                sleep(Duration::from_micros(click_speed as u64));
-                MouseButton::RightButton.release();
+    NumpadPlusKey.bind(enclose!((left_click_toggle=> right_click_toggle) move || {
+            MouseButton::RightButton.press();
+            sleep(Duration::from_micros(click_speed as u64));
+            MouseButton::RightButton.release();
 
-                right_click_toggle
-                    .to_owned()
-                    .lock()
-                    .unwrap()
-                    .replace(Box::new(false));
-            }
-        }),
-    );
-    //Numpad1Key.bind(|| {
-    //TODO: medium speed for held key check should be a config option?
-    // MouseKeyFast.bind(enclose!((is_numlock_on=>is_numlock_on_fast)move || {
-    //         //set fast speed
-    //         is_fast.to_owned().lock().unwrap().replace(Box::new(true));
-    //         // fast is not modal for ergonomics.
-    //         while MouseKeyFast.is_pressed() {
-    //             sleep(Duration::from_micros(medium_speed as u64));
-    //             continue;
-    //         }
-    //         is_fast.to_owned().lock().unwrap().replace(Box::new(false));
-    // }));
+            right_click_toggle
+                .to_owned()
+                .lock()
+                .unwrap()
+                .replace(Box::new(false));
+    }));
 
-    //same as fast for slow using enter key
     MouseKeySlow.bind(enclose!((is_slow)move || {
         is_slow.to_owned().lock().unwrap().replace(Box::new(true));
     }));
@@ -335,52 +362,56 @@ fn main() {
         is_fast.to_owned().lock().unwrap().replace(Box::new(false));
     }));
 
-    //TODO: numlock key is unreliable due to nkro and speed
     //toggle is numlock on each time num lock key is pressed
     // MouseKeyActivate.bind(move || {
-    // NumLockKey.bind(enclose!((is_numlock_on) move || {
-    //     let cur_value = **is_numlock_on.clone().lock().unwrap().borrow();
-    //     is_numlock_on
-    //         .to_owned()
-    //         .lock()
-    //         .unwrap()
-    //         .replace(Box::new(!cur_value));
-    // }));
+    MouseKeyNumlock.bind(enclose!((is_numlock_on)move || {
+        let cur_value = **is_numlock_on.clone().lock().unwrap().borrow();
+        is_numlock_on
+            .to_owned()
+            .lock()
+            .unwrap()
+            .replace(Box::new(!cur_value));
+    }));
+    MouseKeySlash.bind(enclose!((is_rat_on) move || {
+        let cur_value = **is_rat_on.clone().lock().unwrap().borrow();
+        is_rat_on
+            .to_owned()
+            .lock()
+            .unwrap()
+            .replace(Box::new(!cur_value));
+    }));
 
     //Numpad5Key.bind(|| {
     MouseKeyMiddle.bind(
-        enclose!((is_numlock_on=>is_numlock_on_middle, left_click_toggle) move || {
-            if *is_numlock_on_middle.lock().unwrap().borrow().clone() {
+        enclose!((is_numlock_on, is_rat_on, left_click_toggle) move || {
                 //toggle left click
-                //let cur_value = *left_click_active.lock().unwrap().borrow().clone();
-                //if cur_value.clone() {
-                MouseButton::LeftButton.press();
-                sleep(Duration::from_micros(click_speed as u64));
-                MouseButton::LeftButton.release();
-                left_click_toggle
-                    .to_owned()
-                    .lock()
-                    .unwrap()
-                    .replace(Box::new(true));
-                //} else {
-                //    MouseButton::RightButton.press();
-                //    sleep(Duration::from_micros(10));
-                //    MouseButton::RightButton.release();
-                //}
-            }
+                if **is_rat_on.clone().lock().unwrap().borrow() {
+                    MouseButton::LeftButton.press();
+                    sleep(Duration::from_micros(click_speed as u64));
+                    MouseButton::LeftButton.release();
+                    left_click_toggle
+                        .to_owned()
+                        .lock()
+                        .unwrap()
+                        .replace(Box::new(true));
+                } else if !**is_numlock_on.clone().lock().unwrap().borrow() {
+                    &KEYBD_DEVICE.lock().unwrap().press(&keyboard::Key::_5).unwrap();
+                    &KEYBD_DEVICE.lock().unwrap().synchronize();
+                    &KEYBD_DEVICE.lock().unwrap().release(&keyboard::Key::_5).unwrap();
+                    &KEYBD_DEVICE.lock().unwrap().synchronize();
+                }
         }),
     );
+
     //TODO: change these names in input
     MouseKeyClickToggle.bind(
-        enclose!((is_numlock_on=>is_numlock_on_plus,left_click_toggle=>left_click_hold)move || {
-            if **is_numlock_on_plus.lock().unwrap().borrow() {
-                //hold left click, released by another 5 left click
+        enclose!((is_numlock_on, is_rat_on, left_click_toggle=>left_click_hold) move ||{
+                //hold left click. released by another left click
                 if *left_click_hold.lock().unwrap().borrow().clone() {
                     MouseButton::LeftButton.press();
                 } else {
                     //right
                     MouseButton::RightButton.press();
-                }
             }
         }),
     );
